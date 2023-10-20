fcbba6aa0d4e778d282960f4d6f1de992b692c29
OmeshSachethana <sachethana12@gmail.com>
2023-10-20 17:19:47 +0530

    added carousel and images

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -24,7 +25,37 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: Center(child: Text("Logged In as " + user.email!)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Logged In as " + user.email!),
+            SizedBox(height: 20), // Add some spacing
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 170.0,
+              ),
+              items: [
+                'lib/images/image1.jpg',
+                'lib/images/image2.jpeg',
+                'lib/images/image3.jpg'
+              ].map((path) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(color: Colors.amber),
+                      child: Image.asset(
+                          path), // Use the path from the map function
+                    );
+                  },
+                );
+              }).toList(),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
